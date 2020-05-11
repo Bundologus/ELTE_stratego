b23a1d1487531a05babbe2cc896148848adc6a13
Perlaki Andras [CBSDZH] <cbsdzh@inf.elte.hu>
2020-05-12 01:05:57 +0200

    sliding title partially ready

---
+++ siteBuild/index.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+  const left = document.querySelector(".header-text-left");
+  const right = document.querySelector(".header-text-right");
+  right.classList.add("hidden");
+  left.classList.add("hidden");
+}, 10000);
